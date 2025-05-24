a536571a10256ce4454e23bc923068976069ebab
Devoo <fired9229@gmail.com>
2025-05-24 16:54:12 +0700

    Update Sheep.php

--- src/tgwaste/Mobs/Entities/Sheep.php
+++ src/tgwaste/Mobs/Entities/Sheep.php
@@ -9,4 +9,12 @@ use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
 class Sheep extends MobsEntity {
 	const TYPE_ID = EntityIds::SHEEP;
 	const HEIGHT = 1.3;
+	private bool $sheared = false;
+
+public function isSheared(): bool {
+    return $this->sheared;
+}
+
+public function setSheared(bool $sheared): void {
+    $this->sheared = $sheared;
 }
