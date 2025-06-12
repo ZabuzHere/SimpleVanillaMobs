8a45808985b0216614bab56013de1b4d9ece5437
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2025-06-12 10:10:15 +0700

    replace itemlds with itemtypelds to match the latest version

--- src/tgwaste/Mobs/Listener/FeedListener.php
+++ src/tgwaste/Mobs/Listener/FeedListener.php
@@ -4,7 +4,8 @@ namespace tgwaste\Mobs\Listener;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerEntityInteractEvent;
-use pocketmine\item\ItemIds;
+// use pocketmine\item\ItemIds;
+use pocketmine\item\ItemTypeIds;
 use pocketmine\world\particle\HeartParticle;
 use pocketmine\world\particle\AngryVillagerParticle;
 use tgwaste\Mobs\Entities\Chicken;
@@ -20,34 +21,37 @@ class FeedListener implements Listener {
         $item = $player->getInventory()->getItemInHand();
         $entity = $event->getEntity();
 
+        // Itemlds -> ItemTypeIds
+        // getId -> getTypeId
+
         // === Chicken ===
-        if ($entity instanceof Chicken && $item->getId() === ItemIds::WHEAT_SEEDS) {
+        if ($entity instanceof Chicken && $item->getTypeId() === ItemTypeIds::WHEAT_SEEDS) {
             $this->tryFeed($player, $entity, $item);
         }
 
         // === Wolf ===
-        if ($entity instanceof Wolf && in_array($item->getId(), [
-            ItemIds::BEEF, ItemIds::COOKED_BEEF, ItemIds::CHICKEN, ItemIds::COOKED_CHICKEN,
-            ItemIds::PORKCHOP, ItemIds::COOKED_PORKCHOP, ItemIds::ROTTEN_FLESH
+        if ($entity instanceof Wolf && in_array($item->getTypeId(), [
+            ItemTypeIds::BEEF, ItemTypeIds::COOKED_BEEF, ItemTypeIds::CHICKEN, ItemTypeIds::COOKED_CHICKEN,
+            ItemTypeIds::PORKCHOP, ItemTypeIds::COOKED_PORKCHOP, ItemTypeIds::ROTTEN_FLESH
         ])) {
             $this->tryFeed($player, $entity, $item);
         }
 
         // === Pig ===
-        if ($entity instanceof Pig && in_array($item->getId(), [
-            ItemIds::CARROT, ItemIds::POTATO, ItemIds::BEETROOT
+        if ($entity instanceof Pig && in_array($item->getTypeId(), [
+            ItemTypeIds::CARROT, ItemTypeIds::POTATO, ItemTypeIds::BEETROOT
         ])) {
             $this->tryFeed($player, $entity, $item);
         }
 
         // === Sheep ===
-        if ($entity instanceof Sheep && $item->getId() === ItemIds::WHEAT) {
+        if ($entity instanceof Sheep && $item->getTypeId() === ItemTypeIds::WHEAT) {
             $this->tryFeed($player, $entity, $item);
         }
 
         // === Horse ===
-        if ($entity instanceof Horse && in_array($item->getId(), [
-            ItemIds::WHEAT, ItemIds::APPLE, ItemIds::SUGAR, ItemIds::GOLDEN_APPLE
+        if ($entity instanceof Horse && in_array($item->getTypeId(), [
+            ItemTypeIds::WHEAT, ItemTypeIds::APPLE, ItemTypeIds::SUGAR, ItemTypeIds::GOLDEN_APPLE
         ])) {
             $this->tryFeed($player, $entity, $item);
         }
